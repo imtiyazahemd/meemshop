92eed34a228d3b2509ed2a0d2b74e87f6caadbd4
imtiyazahemd <82605775+imtiyazahemd@users.noreply.github.com>
2021-10-16 01:18:58 -0700

    Add files via upload

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,7 +2,7 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
-    const api = process.env.API_URL;
+    const api = '/api/v1';
     return expressJwt({
         secret:'my-zaid',
         algorithms: ['HS256'],
